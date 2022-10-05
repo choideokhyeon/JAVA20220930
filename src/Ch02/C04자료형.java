e962bf28ca8a7a967413b43dc3673a507be5dfd0
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-10-05 12:39:38 +0900

    22-10-15

---
+++ src/Ch02/C04자료형.java
@@ -0,0 +1,31 @@
+package Ch02;
+
+public class C04자료형 {
+
+	public static void main(String[] args) {
+		//-------------------------
+		//	정수
+		//-------------------------
+		//int : 4byte 정수, 기본자료형
+		//00000000 00000000 00000000 00000000
+		
+//		int n1 = 0b10101101;	//2진수
+//		int n2 = 173;			//10진수
+//		int n3 = 0255;			//8진수
+//		int n4 = 0xad;			//16진수
+//		System.out.printf("%d %d %d %d\n", n1, n2, n3, n4);
+		
+		
+		//byte : 1byte 정수, 부호 o
+		//00000000
+		
+		byte n1 = -128;
+		byte n2 = -30;
+		byte n3 = 30;
+		byte n4 = 127;
+//		byte n5 = 128;	//ERROR
+		
+		
+	}
+
+}
--- src/ChPractice/Practice2.java
+++ src/ChPractice/Practice2.java
@@ -1,5 +1,7 @@
 package ChPractice;
 
+import java.util.Scanner;
+
 public class Practice2 {
 
 	public static void main(String[] args)
@@ -71,7 +73,148 @@ public class Practice2 {
 //			}
 //			System.out.println();
 //		}
-
+		
+		
+		
+//		입력값 받아 피라미드 만들기
+//		int i;
+//		int j;
+//		int k;
+//		int input;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("피라미드의 층을 입력하세요 : ");
+//		input = sc.nextInt();
+//		
+//		for(i = 0; i < input; i++)
+//		{
+//			for(j = 0; j < input - i - 1; j++)
+//			{
+//				System.out.print(" ");
+//			}
+//			for(k = 0; k < 2 * i + 1; k++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
+//		sc.close();
+		
+		
+		
+//		입력값 받아 역피라미드 만들기
+//		int i;
+//		int j;
+//		int k;
+//		int input;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("피라미드의 층을 입력하세요 : ");
+//		input = sc.nextInt();
+//		
+//		for(i = 0; i < input; i++)
+//		{
+//			for(j = 0; j < i; j++)
+//			{
+//				System.out.print(" ");
+//			}
+//			for(k = 0; k < 2 * (input - i) - 1 ; k++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+//		다이아몬드 만들기
+//		int i;
+//		int j;
+//		int k;
+//		
+//			for(i = 0; i < 7; i++)
+//			{
+//				if(i < 4)
+//				{
+//					for(j = 0; j < 3 - i; j++)
+//					{
+//						System.out.print(" ");
+//					}
+//					for(k = 0; k < 2 * i + 1; k++)
+//					{
+//						System.out.print("*");
+//					}
+//					System.out.println();
+//				}
+//				else if(i >= 4)
+//				{
+//					for(j = 0; j < i - 3; j++)
+//					{
+//						System.out.print(" ");
+//					}
+//					for(k = 0; k < 13 - 2 * i; k++)
+//					{
+//						System.out.print("*");
+//					}
+//					System.out.println();
+//				}
+//			}
+			
+			
+			
+//		입력값 받아서 다이아몬드 만들기
+		int i;
+		int j;
+		int k;
+		int input;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("다이아몬드의 중간높이를 입력하세요 : ");
+		input = sc.nextInt();
+		
+		for(i = 0; i < (2 * input) - 1; i++)
+		{
+			if(i < input)
+			{
+				for(j = 0; j < input - i - 1; j++)
+				{
+					System.out.print(" ");
+				}
+				for(k = 0; k < 2 * i + 1; k++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			
+			else if(i == input)
+			{
+				for(j = 0; j < i - input + 1; j++)
+				{
+					System.out.print(" ");
+				}
+				for(k = 0; k < 2 * i - 3; k++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			
+			else
+			{
+				for(j = 0; j < i - input + 1; j++)
+				{
+					System.out.print(" ");
+				}
+				for(k = 0; k < (4 * input - 3) - (2 * i); k++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+		
+		
+		
+		sc.close();
 	}
 
 }
